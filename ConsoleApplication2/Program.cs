7191e37618b6ad529a67a8a4f3aecdaa350a1bc7
jjfw0008 <apple_jazz@126.com>
2013-12-25 22:20:16 +0800

    王以浦更改

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication2
         {
             //helloworld //FirstBranch Insert //Develop Branch 并发测试 因为修改了相同祖先 这一模块中的内容 所以合并 需要选择
 
-
+            //王以浦 潜入的版本 
             //ImporttentBranch Create
         }
     }
